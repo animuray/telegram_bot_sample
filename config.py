ef67fd8876b03fe02ccae4644e660a4042b70fb1
animuray <urakov.az.sky@gmail.com>
2025-05-13 18:41:59 +0300

    some changes

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 TOKEN = os.environ.get("BOT_TOKEN")
-admin_id = os.environ.get("admin_id")
+admins_id = os.environ.get("admin_id")
